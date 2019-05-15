4fd832b64d16f0b1c35762a38752f5bda3264932
Jérôme Wiedemann <jerome@wnetworks.org>
2019-05-16 01:21:51 +0200

    Use terser and fix #157

--- rollup.debug.config.js
+++ rollup.debug.config.js
@@ -2,14 +2,13 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: ['src/button-card.ts'],
   output: {
     dir: './dist',
     format: 'es',
-    sourcemap: false,
+    sourcemap: true,
   },
   plugins: [
     resolve(),
@@ -17,18 +16,5 @@ export default {
     json(),
     babel({
       exclude: 'node_modules/**',
-    }),
-    terser({
-      output: {
-        comments: function (node, comment) {
-          var text = comment.value;
-          var type = comment.type;
-          if (type == "comment2") {
-            // multiline comment
-            return /@preserve|@license|@cc_on/i.test(text);
-          }
-        }
-      }
-    }),
-  ],
+    })],
 };
