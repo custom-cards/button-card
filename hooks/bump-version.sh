d69220fbfff22770fc9657c5c5185690a701144a
Jérôme Wiedemann <jerome@wnetworks.org>
2019-08-11 18:41:00 +0200

    Fix version script

--- hooks/bump-version.sh
+++ hooks/bump-version.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 VERSION=$(jq -r .version package.json)
 
-cat <<EOF >src/const.ts
-export const BUTTON_CARD_VERSION = '${VERSION}'
+cat <<EOF >src/version-const.ts
+export const BUTTON_CARD_VERSION = '${VERSION}';
 EOF
