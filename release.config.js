2d3d690b0f8527455b484ca04b96fc9c479a1b11
Jérôme Wiedemann <jerome@wnetworks.org>
2021-02-12 17:21:09 +0000

    chore(ci): Update CI with semantic-release

---
+++ release.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-template-curly-in-string */
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'doc', hidden: false, section: 'Documentation' },
+            { type: 'docs', hidden: false, section: 'Documentation' },
+            { type: 'chore', hidden: true, section: 'Chores' },
+          ],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'README.md', 'package.json', 'yarn.lock'],
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/*.js',
+      },
+    ],
+  ],
+  preset: 'conventionalcommits',
+  branches: [{ name: 'master' }, { name: 'dev', channel: 'beta', prerelease: true }],
+};
