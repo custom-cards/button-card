2f87f37a54c43c074eaafe9e5aba7d5e9032744d
Jérôme Wiedemann <jerome@wnetworks.org>
2025-08-29 09:31:13 +0000

    Fix lock not displayed on safari and lock misplaced, sometimes

--- src/styles.ts
+++ src/styles.ts
@@ -431,6 +431,10 @@ export const styles = css`
     grid-template-rows: 1fr min-content min-content;
   }
 
+  #aspect-ratio {
+    position: relative;
+  }
+
   [style*='--aspect-ratio'] > :first-child {
     width: 100%;
   }
